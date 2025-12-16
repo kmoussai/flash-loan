f0028163028ac43b00e57e4cd35a4d0a383a2f78
khalid moussaid <k.moussaid@outlook.com>
2025-12-16 13:28:42 +0100

    update Payeer name to zum rails

--- src/app/admin/components/ContractViewer.tsx
+++ src/app/admin/components/ContractViewer.tsx
@@ -270,7 +270,7 @@ export default function ContractViewer({
     const signatureDateStaff = formatDate(contractData.staff_signed_at)
     const personalService = true
     const businessService = false
-    const payeeName = 'Accept Pay Global (Payee)'
+    const payeeName = ' Zūm Rails Inc'
 
     const borrowerFullName =
       [terms.first_name, terms.last_name].filter(Boolean).join(' ').trim() ||
